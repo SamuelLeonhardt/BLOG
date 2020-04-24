57d2bb93efed89224d4069c85fd78f4d249b5299
Alorzin <samuel_leonhardt@hotmail.com>
2020-04-24 18:17:02 -0300

    small update

---
+++ content/blog/curiosidades-sobre-o-senhor-dos-aneis/index.md
@@ -0,0 +1,22 @@
+---
+title: Curiosidades sobre a trilogia de o Senhor dos Anéis
+date: "2020-04-25T22:12:03.284Z"
+description: "Senhor dos Anéis"
+---
+
+> Texto não autoral, retirado do site: https://www.estrelando.com.br/foto/2018/12/08/confira-12-curiosidades-sobre-a-trilogia-de-o-senhor-dos-aneis-212446/foto-3
+
+
+## O Universo
+
+![Palavras do um anel refletindo na face do Frodo](./frodo-face-ring.jpg)
+
+O Senhor dos Anéis é uma das franquias mais bem-sucedidas da história do cinema. A história gira em torno de Frodo Baggins que, após se ver em propriedade de um poderoso anel que poderia destruir a humanidade, resolve destruir a joia, com a ajuda de alguns companheiros. Ao longo de A Sociedade do Anel, As Duas Torres e O Retorno do Rei acompanhamos esta jornada que conquistou diversos prêmios e arrecadou bilheterias impressionantes ao redor do mundo. Veja a seguir curiosidades sobre a franquia!
+
+### Improviso é importante? Faz história?
+
+![Gandalf em Moria](./gandalf.jpg)
+
+- Também no primeiro filme, quando Gandalf chega à casa de Bilbo Baggins, ele sem querer bate a testa em um pedaço do teto. O momento não foi proposital e Ian McKellen continuou a cena, como se nada tivesse acontecido. O diretor, Peter Jackson, depois o elogiou, afirmando que o astro fez um ótimo trabalho atuando por meio do erro.
+
+[frodo met the ring](https://en.wikipedia.org/wiki/Salted_duck_egg)
---
+++ gatsby-config.js
@@ -0,0 +1,116 @@
+module.exports = {
+  siteMetadata: {
+    title: `RW`,
+    author: {
+      name: `Renan Wink`,
+      summary: `um aprendiz da vida`,
+    },
+    description: `Descobrindo o universo dentro de mim.`,
+    siteUrl: `https://github.com/samuelleonhardt`,
+    social: {
+      twitter: `Winkrenan`,
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-image`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Renan Wink`,
+        short_name: `RW`,
+        start_url: `/`,
+        background_color: `#606060`,
+        theme_color: `#909090`,
+        display: `minimal-ui`,
+          icon: `content/assets/gatsby-icon.png`,
+      },
+    },
+    //-------------------- Title, description e keywords ---------------------
+    `gatsby-plugin-react-helmet`,
+    //----------------------------------------------------------------------------
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    //---------------------- FONTE DO SITE ---------------------------------------
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Montserrat`,
+            variants: [`300`, `400`, `600`, `700`, `900`]
+          },
+        ],
+      },
+    },
+    //-----------------------IMPEDE A GERAÇÃO DE PÁGINAS-------------------------------
+
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        ignore: [`**/styles.js`], 
+      },
+    },
+    //----------------------------------------------------------------------------
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: process.env.NODE_ENV !== 'production',
+      },
+    },
+    //----------------------------------------------------------------------------
+      
+    //-------------------- SALVA O SITE COMO APLICATIVO --------------------------  
+    `gatsby-plugin-offline`,
+    //----------------------------------------------------------------------------
+  ],
+}
